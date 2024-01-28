f4e8d468dbf99d0e1f1ff4448a27f32d582becff
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2024-01-28 11:16:57 +0200

    @

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
-enum Color {
+use std::process::id;
+
+enum Message {
+    Hello {id: i32},
+}
+
+
+fn main() {
+    let msg = Message::Hello {id: 5};
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello {id: 10..=12} => {
+            println!("Found an id in another range")
+        }
+        Message::Hello {id} => println!("Found some other id: {}", id)
+    }
+}
+
+
+
+
+/*enum Color {
     Rgb(i32, i32, i32),
     Hsv(i32, i32, i32),
 }
@@ -41,7 +65,7 @@ fn main() {
     }*/
 
 }
-
+*/
 
 
 /*fn main() {
